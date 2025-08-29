5ee83d571aee0b6b616bffe94aff5257a11b95d0
trisastra04 <trisastra04@gmail.com>
2025-08-30 00:39:10 +0700

    Add files via upload

---
+++ script_absensi.js
@@ -0,0 +1,5 @@
+
+document.getElementById("absensiForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  document.getElementById("status").innerText = "Data absensi berhasil dikirim!";
+});
