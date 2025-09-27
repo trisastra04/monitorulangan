25234139f8160074c78eb733ce590bd6843d5919
trisastra04 <trisastra04@gmail.com>
2025-09-27 15:21:41 +0700

    Delete script_absensi.js

--- script_absensi.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("absensiForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("status").innerText = "Data absensi berhasil dikirim!";
-});
